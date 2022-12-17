cff7261f5f606dc6a87f8cd05c6fcaed4840bd7c
Alexey Gvozdev <aonefan@gmail.com>
2022-12-17 23:17:23 +0300

    Add wait

--- Ghole.py
+++ Ghole.py
@@ -3,16 +3,18 @@ import cv2
 from playsound import playsound
 
 playsound(
-        '/Users/gvozdevalexey/Downloads/TK_from_Ling_Tosite_Sigure_-_UNRAVEL_ost_Tokyo_Ghoul_Tokijjskijj_gul_OP_64191680.mp3', False)
+    '/Users/gvozdevalexey/Downloads/TK_from_Ling_Tosite_Sigure_-_UNRAVEL_ost_Tokyo_Ghoul_Tokijjskijj_gul_OP_64191680.mp3',
+    False)
 
 a = 1000
 while a >= 0:
     a = a - 7
-    time.sleep(0.5)
+    time.sleep(0.1)
     print(a)
-
-img = cv2.imread('/Users/gvozdevalexey/Downloads/shounen-red-eyes-human-monster-tokyo-ghoul-ken-kaneki-ghoul.jpg', 0)
-cv2.imshow('', img)
-cv2.waitKey(0)
-
-
+    if a < 0:
+        img = cv2.imread(
+            '/Users/gvozdevalexey/Downloads/shounen-red-eyes-human-monster-tokyo-ghoul-ken-kaneki-ghoul.jpg', 0)
+        cv2.imshow('', img)
+        cv2.waitKey(1)
+        time.sleep(5)
+quit()
